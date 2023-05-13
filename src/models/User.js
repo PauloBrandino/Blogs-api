3c2872173c47e4ec78edd081e35618c792fb5867
PauloBrandino <contatobrandino@gmail.com>
2023-05-13 09:18:00 -0300

    Create user model

---
+++ src/models/User.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require("sequelize");
+
+const UserSchema = (sequelize, DataTypes) => {
+    const UserTable = sequelize.define('User', {
+        id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+        displayName: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.STRING,
+        image: DataTypes.STRING
+    },
+    {
+        tableName: 'users',
+        underscored: true,
+        timestamps: false,
+    });
+    return UserTable;
+};
+
+module.exports = UserSchema;
